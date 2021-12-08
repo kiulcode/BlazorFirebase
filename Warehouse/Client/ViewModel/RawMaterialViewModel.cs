46f22b2f6b2861b6a0cbc7aa1662a4fed1fe2119
Luis Miguel Uriarte Llaja <hello.kiulcode@gmail.com>
2021-12-08 12:35:24 -0500

    ✨ Finalización del CRUD materia prima

---
+++ Warehouse/Client/ViewModel/RawMaterialViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Google.Cloud.Firestore;
+
+namespace Warehouse.Client.ViewModel
+{
+    public class RawMaterialViewModel
+    {
+        public string ID { get; set; }
+
+        [Required(ErrorMessage = "⚠️ Campo requerido")]
+        public string InputId { get; set; }
+
+        [Required(ErrorMessage = "⚠️ Campo requerido")]
+        public int IncomingQuantity { get; set; }
+
+        [Required(ErrorMessage = "⚠️ Campo requerido")]
+        public int OutgoingQuantity { get; set; }
+
+        [Required(ErrorMessage = "⚠️ Campo requerido")]
+        public int Waste { get; set; }
+
+        [Required(ErrorMessage = "⚠️ Campo requerido")]
+        public int Reusable { get; set; }
+
+        public string CreateTime { get; set; }
+        
+        public DateTime? CreateTimeDate { get; set; } = DateTime.Today;
+    }
+}
