df91ec3b55274425f58225ecda80caa578f4b97b
Luis Miguel Uriarte Llaja <hello.kiulcode@gmail.com>
2021-12-15 02:16:55 -0500

    ✨ Mejoramiento del codigo

---
+++ Warehouse/Client/ViewModel/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Warehouse.Client.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "⚠️ Campo requerido")]
+        [EmailAddress(ErrorMessage = "⚠️ Formato de email inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "⚠️ Campo requerido")]
+        public string Password { get; set; }
+    }
+}
